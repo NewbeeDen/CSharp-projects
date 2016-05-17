08351a281fe1da198385b8061ad264d82f08f67b
Denis Lazarenko <xden89@gmail.com>
2016-05-17 16:22:35 +0300

    Project for saving file in network

---
+++ NetworkTransfer/NetworkTransfer/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace NetworkTransfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum ResourceScope
+        {
+            RESOURCE_CONNECTED = 1,
+            RESOURCE_GLOBALNET,
+            RESOURCE_REMEMBERED,
+            RESOURCE_RECENT,
+            RESOURCE_CONTEXT
+        }
+
+        public enum ResourceType
+        {
+            RESOURCETYPE_ANY,
+            RESOURCETYPE_DISK,
+            RESOURCETYPE_PRINT,
+            RESOURCETYPE_RESERVED
+        }
+
+        public enum ResourceUsage
+        {
+            RESOURCEUSAGE_CONNECTABLE = 0x00000001,
+            RESOURCEUSAGE_CONTAINER = 0x00000002,
+            RESOURCEUSAGE_NOLOCALDEVICE = 0x00000004,
+            RESOURCEUSAGE_SIBLING = 0x00000008,
+            RESOURCEUSAGE_ATTACHED = 0x00000010,
+            RESOURCEUSAGE_ALL = (RESOURCEUSAGE_CONNECTABLE | RESOURCEUSAGE_CONTAINER | RESOURCEUSAGE_ATTACHED),
+        }
+
+        public enum ResourceDisplayType
+        {
+            RESOURCEDISPLAYTYPE_GENERIC,
+            RESOURCEDISPLAYTYPE_DOMAIN,
+            RESOURCEDISPLAYTYPE_SERVER,
+            RESOURCEDISPLAYTYPE_SHARE,
+            RESOURCEDISPLAYTYPE_FILE,
+            RESOURCEDISPLAYTYPE_GROUP,
+            RESOURCEDISPLAYTYPE_NETWORK,
+            RESOURCEDISPLAYTYPE_ROOT,
+            RESOURCEDISPLAYTYPE_SHAREADMIN,
+            RESOURCEDISPLAYTYPE_DIRECTORY,
+            RESOURCEDISPLAYTYPE_TREE,
+            RESOURCEDISPLAYTYPE_NDSCONTAINER
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private class NETRESOURCE
+        {
+            public ResourceScope dwScope = 0;
+            public ResourceType dwType = 0;
+            public ResourceDisplayType dwDisplayType = 0;
+            public ResourceUsage dwUsage = 0;
+            public string lpLocalName = null;
+            public string lpRemoteName = null;
+            public string lpComment = null;
+            public string lpProvider = null;
+        }
+
+        [DllImport("mpr.dll")]
+        private static extern int WNetAddConnection2(NETRESOURCE lpNetResource, string lpPassword, string lpUsername, int dwFlags);
+
+        [DllImport("mpr.dll")]
+        private static extern int WNetCancelConnection2(string lpName, int dwFlags, int fForce);
+
+        public int MapShare(string unc, string user, string password)
+        {
+            NETRESOURCE myNetResource = new NETRESOURCE();
+            myNetResource.lpRemoteName = unc;
+            myNetResource.lpLocalName = null;
+            myNetResource.lpProvider = null;
+            int result = WNetAddConnection2(myNetResource, password, user, 0);
+            return result;
+        }
+
+        public int DeleteShare(string path)
+        {
+            int result = WNetCancelConnection2(path, 0, 1);
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NETRESOURCE rc = new NETRESOURCE();
+            rc.dwType = 0x00000000;
+            rc.lpRemoteName = @"\\192.168.1.240\Users\";
+            rc.lpLocalName = null;
+            rc.lpProvider = null;
+            int ret = WNetAddConnection2(rc, "11111", "Донбас", 0);
+            FileStream fs = new FileStream(@"\\192.168.1.240\Users\123.txt", FileMode.OpenOrCreate);
+            byte[] data = System.Text.Encoding.Unicode.GetBytes(textBox1.Text);
+            fs.Write(data, 0, data.Length);
+            fs.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
